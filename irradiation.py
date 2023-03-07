Fix for long irradiation calculation: uniform rad-maps

irradiation.py
@@ -24,11 +24,38 @@ def irradiationcalc(times, targetlat, targetlon):
     return irradiation
 
 
-def irradiationmap(boundary, times, altitudes):
+def irradiationmap_old(boundary, times, altitudes):
     irradmap = np.empty(shape=(len(times), altitudes.shape[0], altitudes.shape[1]))
     for idxs, _ in np.ndenumerate(irradmap):
         rads = irradiationcalc(times, boundary['CH_S'] + idxs[0], boundary['CH_W'] + idxs[1])
         irradmap[:, idxs[0]-1, idxs[1]] = rads
+
+    return irradmap
+
+
+def irradiationmap(boundary, times, altitudes):
+    irradmap = np.empty(shape=(len(times), altitudes.shape[0], altitudes.shape[1]))
+    lat_SW, lon_SW = lv_to_wgs84(boundary['CH_S'], boundary['CH_W'], type='lv95')
+    lat_NW, lon_NW = lv_to_wgs84(boundary['CH_N'], boundary['CH_W'], type='lv95')
+    lat_SE, lon_SE = lv_to_wgs84(boundary['CH_S'], boundary['CH_E'], type='lv95')
+    lat_NE, lon_NE = lv_to_wgs84(boundary['CH_N'], boundary['CH_E'], type='lv95')
+    for idx, time in enumerate(times):
+        time = time.tz_localize(timezone('Europe/Zurich'))
+
+        altSW = solar.get_altitude(lat_SW, lon_SW, time)
+        radSW = radiation.get_radiation_direct(time, altSW)
+
+        altNW = solar.get_altitude(lat_NW, lon_NW, time)
+        radNW = radiation.get_radiation_direct(time, altNW)
+
+        altSE = solar.get_altitude(lat_SE, lon_SE, time)
+        radSE = radiation.get_radiation_direct(time, altSE)
+
+        altNE = solar.get_altitude(lat_NE, lon_NE, time)
+        radNE = radiation.get_radiation_direct(time, altNE)
+
+        irradmap[idx, :, :] = np.mean([radSW, radNW, radSE, radNE])
+
 
     return irradmap
 